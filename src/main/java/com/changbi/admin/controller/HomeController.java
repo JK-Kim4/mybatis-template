2051227647054c1f25d41611ffa20f9c853cf03f
jw.kim <jongbell4@gmail.com>
2022-05-17 09:57:28 +0900

    - jenkins web-hook test Date : 2022. 05. 17 Author : KJW

--- src/main/java/com/changbi/admin/controller/HomeController.java
+++ src/main/java/com/changbi/admin/controller/HomeController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @AllArgsConstructor
@@ -20,4 +21,9 @@ public class HomeController {
         return "contents/user/login";
     }
 
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
 }
